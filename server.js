38f668434f3fe17a78b42e30c8cfbd17865f3b38
Tharun <77895048+tharun-vemula@users.noreply.github.com>
2022-02-26 01:11:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(postRouter);
 mongoose
     .connect(MONGO_URI)
     .then(result => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
