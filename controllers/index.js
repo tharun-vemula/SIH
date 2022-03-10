916c5d3e4219a46618ece8aac372b0d12b1b4857
tharunv2122 <tharunvind@gmail.com>
2022-03-11 01:13:10 +0530

    index primary changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Post = require('../models/post');
 
 
@@ -14,20 +13,3 @@ exports.compose = (req, res) => {
     res.render('posts/compose', {pageTitle : 'Compose', user: req.session.user});
 }
 
-=======
-const Post = require('../models/post');
-
-
-exports.getHome = (req , res) => {
-    Post.find()
-        .then(posts => {
-            res.render('posts/index', {posts: posts, pageTitle : 'Raven', user: req.session.user});
-        })
-    
-}
-
-exports.compose = (req, res) => {
-    res.render('posts/compose', {pageTitle : 'Compose', user: req.session.user});
-}
-
->>>>>>> 1c8107c0ce596895385a3fd95d1f29ae28f2dcf0
