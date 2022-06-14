7ee60854e324ecd3bf9e1bdc94999eb36f4f3359
tharunv2122 <tharunvind@gmail.com>
2022-06-14 23:03:24 +0530

    site deploy

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const multer = require('multer');
 const flash = require('connect-flash');
+const {spawn} = require('child_process')
 
 const MONGO_URI = 'mongodb://tharun:F0zvliOynvWjbrKf@cluster0-shard-00-00.iy4kk.mongodb.net:27017,cluster0-shard-00-01.iy4kk.mongodb.net:27017,cluster0-shard-00-02.iy4kk.mongodb.net:27017/sample?ssl=true&replicaSet=atlas-k2o103-shard-0&authSource=admin&retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
 
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes/index');
@@ -54,6 +56,22 @@ app.get('/home', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
+app.get('/try' , (req, res) => {
+    var dataToSend;
+    
+    const python = spawn('python', ['./py-scripts/test-run.py']);
+    
+    python.stdout.on('data', function (data) {
+     console.log('Pipe data from python script ...');
+     dataToSend = data.toString();
+    });
+    
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    res.send(dataToSend)
+    });
+})
+
 app.use(authRouter);
 app.use(indexRouter);
 app.use(postRouter);
@@ -63,7 +81,9 @@ app.use(postRouter);
 mongoose
     .connect(MONGO_URI)
     .then(result => {
-        app.listen(3000);
+        app.listen(PORT, (req,res) =>{
+            console.log("Running")
+        });
     })
     .catch(err => {
         console.log(err);
